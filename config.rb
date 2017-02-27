f03fb79131abe229cd4c80a5548a244d68d37762
Lucas Dohmen <lucas@dohmen.io>
2017-02-27 14:52:14 +0100

    Empty Middleman App

---
+++ config.rb
@@ -0,0 +1,26 @@
+###
+# Page options, layouts, aliases and proxies
+###
+
+# General configuration
+
+# Development-specific configuration
+configure :development do
+end
+
+###
+# Helpers
+###
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+# Build-specific configuration
+configure :build do
+  # Minify CSS on build
+  # activate :minify_css
+end
